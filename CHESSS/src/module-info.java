0781dbcc57f7f47e3bb1d59977e941fac0099de3
sboltnotfound <148108300+sboltnotfound@users.noreply.github.com>
2025-04-12 09:40:39 +0530

    Delete CHESSS directory

--- CHESSS/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module chess {
-	requires transitive javafx.graphics;
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires javafx.base;
-	opens org.openjfx to javafx.fxml;
-	exports org.openjfx;
-	
-}
